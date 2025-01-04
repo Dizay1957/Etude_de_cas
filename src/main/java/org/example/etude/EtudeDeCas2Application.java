2df58c68bc9ae6f3b51e1b621b3f38f41d3ab51d
EL YAZID EL MERZOUKI <businessyaz9011@outlook.com>
2025-01-04 20:28:32 +0100

    first com

---
+++ src/main/java/org/example/etude/EtudeDeCas2Application.java
@@ -0,0 +1,13 @@
+package org.example.etude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtudeDeCas2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EtudeDeCas2Application.class, args);
+    }
+
+}
